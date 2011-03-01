4525f43683d70bd68d80fad49d07b9cf2ad38928
Enric Morales <me@enric.me>
2011-03-01 14:08:07 +0100

    Added Awesome WM configs

---
+++ awesome/vicious/widgets/init.lua
@@ -0,0 +1,14 @@
+---------------------------------------------------
+-- Vicious widgets for the awesome window manager
+---------------------------------------------------
+-- Licensed under the GNU General Public License v2
+--  * (c) 2010, Adrian C. <anrxc@sysphere.org>
+---------------------------------------------------
+
+-- {{{ Configure widgets
+require("vicious.widgets.batpmu")
+require("vicious.widgets.date")
+-- }}}
+
+-- Vicious: widgets for the awesome window manager
+module("vicious.widgets")
