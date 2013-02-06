f7e05fdd42d3890f8f8d888b368dd92d22cfdca1
Enric Morales <me@enric.me>
2013-02-06 18:40:54 +0100

    formatting tweaks

--- awesome/themes/ice/theme.lua
+++ awesome/themes/ice/theme.lua
@@ -2,25 +2,24 @@
 
 theme = {}
 
-theme.font          = "Ubuntu 10"
+theme.font = "Ubuntu 10"
 
-theme.bg_normal     = "#000000b0"
-theme.bg_focus     =  "#073642ff"
-theme.bg_urgent     = "#dc322f90"
-theme.bg_minimize   = "#44444490"
+theme.bg_normal   = "#00000060"
+theme.bg_systray  = theme.bg_normal
+theme.bg_focus    = "#073642ff"
+theme.bg_urgent   = "#dc322f90"
+theme.bg_minimize = "#44444490"
 
-theme.fg_normal     = "#eeeeee"
-theme.fg_focus      = "#ffffff"
-theme.fg_urgent     = "#ffffff"
-theme.fg_minimize   = "#ffffff"
+theme.fg_normal    = "#eeeeee"
+theme.fg_focus     = "#ffffff"
+theme.fg_urgent    = "#ffffff"
+theme.fg_minimize  = "#ffffff"
 
-theme.border_width  = "0"
-theme.border_normal = "#00000080"
-theme.border_focus  = "#00000080"
-theme.border_marked = "#00000080"
-
-
-theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
+-- {{{ Borders
+theme.border_width  = 0
+theme.border_normal = "#00000000"
+theme.border_focus  = "#00000000"
+theme.border_marked = "#00000000"
 
 theme.menu_height = "15"
 theme.menu_width  = "100"
@@ -28,28 +27,29 @@ theme.menu_fg_normal = "#aaaaaa"
 theme.menu_border_color = "#000000ff"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
+basedir = "/usr/share/awesome/themes/default/titlebar/"
+theme.titlebar_close_button_normal = basedir .. "close_normal.png"
+theme.titlebar_close_button_focus  = basedir .. "close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = basedir .. "ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = basedir .. "ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = basedir .. "ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = basedir .. "ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = basedir .. "sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = basedir .. "sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = basedir .. "sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = basedir .. "sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = basedir .. "floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = basedir .. "floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = basedir .. "floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = basedir .. "floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = basedir .. "maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = basedir .. "maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = basedir .. "maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = basedir .. "maximized_focus_active.png"
 
 theme.awesome_icon = awehome .. "themes/ice/awesome16.png"
 
