0e44f85485cfa0746d6753277d28c7e87120e7ed
Enric Morales <me@enric.me>
2013-05-25 16:07:49 +0200

    Cleanup deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 
-if [ -z $XDG_CONFIG_HOME ]; then
-	XDG_CONFIG_HOME="${HOME}/.config"
-fi
+# Check if the XDG CONFIG and DATA location variables are set...
+# ... and create them if they don't exist
+test -z $XDG_CONFIG_HOME && XDG_CONFIG_HOME="${HOME}/.config"
+test -e $XDG_CONFIG_HOME || mkdir $XDG_CONFIG_HOME
 
-if [ -z $XDG_DATA_HOME ]; then
-	XDG_DATA_HOME="${HOME}/.local/share"
-fi
+test -z $XDG_DATA_HOME && XDG_DATA_HOME="${HOME}/.local/share"
+test -e $XDG_DATA_HOME || mkdir -p $XDG_DATA_HOME
 
-if ! [ -e $XDG_CONFIG_HOME ]; then
-	mkdir $XDG_CONFIG_HOME
-fi
-
-if ! [ -e $XDG_DATA_HOME ]; then
-	mkdir -p $XDG_DATA_HOME
-fi
 
 deploy(){
+	# Symlinks the first parameter into the second, as long as
+	# the destination folder doesn't exist.
+
 	if ! [ -e $2 ]; then
-		echo -n "ln -s "${PWD}/$1" "$2"... "
+		echo -n "Linking "$1" to "$2"... "
 		ln -s "${PWD}/$1" "$2"
 		[ $? = 0 ] && echo "OK"
 	else
-		echo "Skipping $1"
+		echo "Warning: $1 already exists, skipping."
 	fi
 }
 
+
 deploy awesome ${XDG_CONFIG_HOME}/awesome
 deploy awesome ${XDG_DATA_HOME}/awesome
+
 deploy mplayer ${HOME}/.mplayer
+
 deploy mutt ${HOME}/.mutt
+
+deploy newsbeuter ${XDG_CONFIG_HOME}/newsbeuter
+
 deploy tmux.conf ${HOME}/.tmux.conf
+
 deploy vifm ${HOME}/.vifm
+
 deploy vim ${HOME}/.vim
 deploy vim/vimrc ${HOME}/.vimrc
+
 deploy wgetrc ${HOME}/.wgetrc
+
+deploy newsbeuter ${XDG_CONFIG_HOME}/newsbeuter
+
 deploy zshrc ${HOME}/.zshrc
