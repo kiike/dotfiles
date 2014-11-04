a6520441b48a2ef992941f0deecdd39526b0ccea
Enric Morales <me@enric.me>
2014-11-04 14:21:54 +0100

    awesome: tweek base16 theme

--- awesome/themes/base16/theme.lua
+++ awesome/themes/base16/theme.lua
@@ -1,6 +1,5 @@
 --
 -- Base16-based theme, translucent
--- by Enric Morales 2014
 --
 
 theme = {}
@@ -9,18 +8,18 @@ theme.font          = "Source Code Pro 10"
 
 theme.bg_normal     = "#15151580"
 theme.bg_focus      = theme.bg_normal
-theme.bg_urgent     = theme.bg_normal
+theme.bg_urgent     = "#fb9fb1FF"
 theme.bg_minimize   = theme.bg_normal
 theme.bg_systray    = theme.bg_normal
 
 theme.fg_normal     = "#d0d0d0FF"
 theme.fg_focus      = "#12cfc0FF"
-theme.fg_urgent     = "#fb9fb1FF"
+theme.fg_urgent     = theme.fg_normal
 theme.fg_minimize   = "#151515ff"
 
 theme.border_width  = 2
-theme.border_normal = "#151515FF"
-theme.border_focus  = "#12cfc0FF"
+theme.border_normal = theme.fg_normal
+theme.border_focus  = theme.fg_focus
 theme.border_marked = "#91231c"
 
 theme.menu_submenu_icon = "/usr/share/awesome/themes/default/submenu.png"
