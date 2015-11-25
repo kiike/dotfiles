71a0afec72a3e7892ab862d535f3514d341db1c4
Enric Morales <me@enric.me>
2015-11-25 13:21:41 +0100

    Add colors as table

--- awesome/themes/gruvbox/theme.lua
+++ awesome/themes/gruvbox/theme.lua
@@ -1,67 +1,53 @@
--- Translucent, solarized theme
---[[ ! Gruvbox colors
-! hard contrast:
-*background: #1d2021
-*foreground: #ebdbb2
-! Black + DarkGrey
-*color0:  #282828
-*color8:  #928374
-! DarkRed + Red
-*color1:  #cc241d
-*color9:  #fb4934
-! DarkGreen + Green
-*color2:  #98971a
-*color10: #b8bb26
-! DarkYellow + Yellow
-*color3:  #d79921
-*color11: #fabd2f
-! DarkBlue + Blue
-*color4:  #458588
-*color12: #83a598
-! DarkMagenta + Magenta
-*color5:  #b16286
-*color13: #d3869b
-! DarkCyan + Cyan
-*color6:  #689d6a
-*color14: #8ec07c
-! LightGrey + White
-*color7:  #a89984
-*color15: #ebdbb2
-
-]]--
+-- Gruvbox-based theme
 
 theme = {}
 
-theme.font          = "Source Code Pro 10"
+theme.font = "Potipoti 10"
 
-theme.bg_normal     = "#1d2021"
+theme.colors = {
+    black =	"#282828",
+    grey_dark = "#928374",
+    red_dark = "#cc241d",
+    red = "#fb4934",
+    green_dark = "#98971a",
+    green = "#b8bb26",
+    yellow_dark = "#d79921",
+    yellow = "#fabd2f",
+    blue_dark = "#458588",
+    blue = "#83a598",
+    magenta_dark = "#b16286",
+    magenta = "#d3869b",
+    cyan_dark = "#689d6a",
+    cyan = "#8ec07c",
+    grey = "#a89984",
+    white = "#ebdbb2"
+    }
+
+
+theme.bg_normal     = theme.colors.black
 theme.bg_focus      = theme.bg_normal
-theme.bg_urgent     = "#cc241d"
+theme.bg_urgent     = theme.colors.red
 theme.bg_minimize   = theme.bg_normal
 theme.bg_systray    = theme.bg_normal
 
-theme.fg_normal     = "#a89984"
-theme.fg_focus      = "#ebdbb2"
+theme.fg_normal     = theme.colors.grey_dark
+theme.fg_focus      = theme.colors.white
 theme.fg_urgent     = theme.fg_normal
 theme.fg_minimize   = theme.fg_normal
 
-theme.border_width  = 2
-theme.border_normal = theme.fg_normal
+theme.border_width  = 4
+theme.border_normal = theme.colors.grey_dark
 theme.border_focus  = theme.fg_focus
-theme.border_marked = "#91231c"
+theme.border_marked = theme.colors.blue
 
-theme.menu_submenu_icon = "/usr/share/awesome/themes/default/submenu.png"
 theme.menu_height = 16
-theme.menu_width  = 120
+theme.menu_width  = 140
 
 theme.wallpaper = "pictures/wallpaper"
 
--- Hide Awesome menu icon
+-- Hide icons
 theme.awesome_icon = nil
-
 theme.icon_theme = nil
-
--- Remove icons from tasklist
 theme.tasklist_disable_icon = true
 
 return theme
