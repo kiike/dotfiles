d5885bc190f483e7634c75b66f0a865e9ea84df7
Enric Morales <me@enric.me>
2017-02-02 18:05:30 +0100

    v2

--- bin/sense.py
+++ bin/sense.py
@@ -14,8 +14,16 @@ import urwid
 DATE_FMT = "%Y-%m-%d %H:%M:%S"
 QUIT_HINT = "Press 'q' to quit"
 BLACKLIST = ("PCH_CHIP_CPU_MAX_TEMP", "PCH_CHIP_TEMP", "PCH_CPU_TEMP",
-             "AUXTIN1", "AUXTIN2", "AUXTIN3", "intrusion1", "intrusion2",
-             "fan1", "fan3", "fan5")
+             "AUXTIN1", "AUXTIN2", "AUXTIN3", "intrusion0", "intrusion1", "intrusion2",
+             "fan3", "fan5", "beep_enable")
+
+PALETTE = (('bg', 'default', 'default'),
+           ('symbol', 'dark gray', 'default'),
+           ('chip', 'dark cyan', 'default'),
+           ('title', 'light green', 'default'),
+           ('date', 'yellow', 'default'),
+           ('quit_hint', 'dark gray', 'default'),
+           ('sensor', 'light cyan', 'default'))
 
 def init_history(chips):
     """
@@ -23,7 +31,10 @@ def init_history(chips):
     """
 
     # Adapted from lm-sensors source code
-    sensor_units = ['', ' RPM', ' °C', '', '', '', '', ' V', ' ', '', '']
+    sensor_units = [' V', ' RPM', ' °C', '', '', '', '', ' V', ' ', '', '', '', '', '']
+    sensor_types = ['in', 'fan', 'temp', 'power', 'energy', 'current',
+                    'humidity', 'max_main', 'vid', 'intrusion', 'max_other',
+                    'beep_enable', 'max', 'unknown']
 
     tree = {}
     for chip in chips:
@@ -32,9 +43,15 @@ def init_history(chips):
             if feature.label in BLACKLIST:
                 continue
             else:
-                unit = sensor_units[feature.type]
+                try:
+                    unit = sensor_units[feature.type]
+                    sensor_type = sensor_types[feature.type]
+                except IndexError:
+                    unit = " ???"
+                    sensor_type = " ???"
+
                 sensor_dict[feature.label] = {}
-                sensor_dict[feature.label]['info'] = {'unit': unit}
+                sensor_dict[feature.label]['info'] = {'unit': unit, 'type': sensor_type}
                 sensor_dict[feature.label]['measurements'] = []
 
         tree[str(chip)] = sensor_dict
@@ -43,14 +60,14 @@ def init_history(chips):
     for cpu in range(psutil.cpu_count()):
         core_id = 'Core #{}'.format(cpu)
         tree['CPU Usage'][core_id] = {}
-        tree['CPU Usage'][core_id]['info'] = {'unit': '%'}
+        tree['CPU Usage'][core_id]['info'] = {'unit': ' %', 'type': 'usage'}
         tree['CPU Usage'][core_id]['measurements'] = []
 
     tree['CPU Frequency'] = {}
     for cpu in range(psutil.cpu_count()):
         core_id = 'Core #{}'.format(cpu)
         tree['CPU Frequency'][core_id] = {}
-        tree['CPU Frequency'][core_id]['info'] = {'unit': ' MHz'}
+        tree['CPU Frequency'][core_id]['info'] = {'unit': ' MHz', 'type': 'freq'}
         tree['CPU Frequency'][core_id]['measurements'] = []
 
     return tree
@@ -82,31 +99,24 @@ def update_history(history, chips):
     return history
 
 
-def format_number(number):
+def update_footer():
+    title = urwid.AttrMap(urwid.Text('sense.py', align='left'), 'title')
+    date = urwid.AttrMap(urwid.Text(time.strftime(DATE_FMT), align='center'), 'date')
+    quit_hint = urwid.AttrMap(urwid.Text(QUIT_HINT, align='right'), 'quit_hint')
+    return urwid.Columns((title, date, quit_hint))
+
+def format_field(number, unit, s_type):
     """
-    Print numbers in a saner way.
+    Make an urwid text out of a number and unit suitable for putting into columns.
     """
-    if number.is_integer():
-        fmt = '{:.0f}'.format(number)
+
+    if s_type in ('fan', 'freq', 'usage', 'temp'):
+        field = '{:>7d}{:<3}'.format(round(number), unit)
     else:
-        fmt = '{:.3f}'.format(number)
+        field = '{:>7.3f}{:<3}'.format(number, unit)
 
-    return fmt
+    return urwid.Text(field)
 
-
-def add_header():
-    header = '{:<6} {:<6} {:<6} {:<6}'
-    header = header.format('cur', 'min', 'max', 'avg')
-    return header
-
-
-def add_footer():
-    footer = "{:<16} {:<16} {:>16}"
-    footer = footer.format('sense.py',
-                           time.strftime(DATE_FMT),
-                           QUIT_HINT)
-
-    return footer
 
 def get_output(history):
     """
@@ -117,7 +127,7 @@ def get_output(history):
 
     for chip in history:
         # Print sensor name
-        out.append(str(chip))
+        out.append(urwid.AttrMap(urwid.Text(str(chip)), 'chip'))
 
         features = history[chip]
         for i, feature in enumerate(features):
@@ -128,29 +138,36 @@ def get_output(history):
             # Print sensor measurements
             measurements = history[chip][feature]['measurements']
             unit = history[chip][feature]['info']['unit']
-            data_fmt = '{:>6}{} {:>6}{} {:>6}{} {:>6.3f}{}'
-            data = data_fmt.format(format_number(measurements[-1]), unit,
-                                   format_number(min(measurements)), unit,
-                                   format_number(max(measurements)), unit,
-                                   statistics.mean(measurements), unit)
-            line = "{} {} {}".format(symbol, feature, data)
+            s_type = history[chip][feature]['info']['type']
+
+            f_cur = format_field(measurements[-1], unit, s_type)
+            f_min = format_field(min(measurements), unit, s_type)
+            f_max = format_field(max(measurements), unit, s_type)
+            f_avg = format_field(statistics.mean(measurements), unit, s_type)
+
+            data_fields = urwid.Columns((f_cur, f_min, f_max, f_avg))
+
+            line = urwid.Columns(((2, urwid.AttrMap(urwid.Text(symbol), 'symbol')),
+                                  (16, urwid.AttrMap(urwid.Text(feature), 'sensor')),
+                                  data_fields))
             out.append(line)
 
-        out.append("")
+        # Show an empty line between sensors
+        out.append(urwid.Text(''))
 
-    return out
+    return urwid.SimpleListWalker([w for w in out])
 
-def updater(loop, body, chips, history):
-    """ Loop that updates the content window. """
+def update_frame(frame, loop, listwalker, chips, history):
+    """ Loop that replaces the frame listwalker in-place. """
     while True:
         history = update_history(history, chips)
-        output = get_output(history)
-        body.set_text("\n".join(output))
+        listwalker[:] = get_output(history)
+        frame.footer = update_footer()
         loop.draw_screen()
         time.sleep(1)
 
 def key_handler(key):
-    """ Handle keys such as Q for quit, etc."""
+    """ Handle keys such as q and Q for quit, etc."""
     if key in ('q', 'Q'):
         raise urwid.ExitMainLoop()
 
@@ -159,19 +176,37 @@ def main():
     Initialize screen, sensors, history and display measurements on the screen.
     """
 
+    commands = urwid.CommandMap().copy()
+    commands['j'] = 'cursor down'
+    commands['k'] = 'cursor up'
+    urwid.CommandMap = commands
+
     # Initialize the sensors and the history
     sensors.init()
     chips = [s for s in sensors.iter_detected_chips()]
     history = init_history(chips)
+    history = update_history(history, chips)
 
-    body = urwid.Text("")
-    fill = urwid.Filler(body, "top")
-    loop = urwid.MainLoop(fill, unhandled_input=key_handler)
+    listwalker = get_output(history)
+    body = urwid.ListBox(listwalker)
 
-    updater_thread = threading.Thread(target=updater,
-                                      args=(loop, body, chips, history))
+    header = (urwid.Text(w, align='center') for w in ('cur', 'min', 'max', 'avg'))
+    header = urwid.Columns(((2, urwid.Text("")),
+                            (16, urwid.Text("")),
+                            urwid.Columns(header)))
 
-    updater_thread.start()
+    footer = update_footer()
+    frame = urwid.Frame(body, header=header)
+
+
+
+    loop = urwid.MainLoop(frame, unhandled_input=key_handler, palette=PALETTE)
+
+    frame_updater = threading.Thread(target=update_frame,
+                                     args=(frame, loop, listwalker,
+                                           chips, history))
+
+    frame_updater.start()
     loop.run()
 
 
