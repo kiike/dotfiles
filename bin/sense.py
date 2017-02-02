f25f1db5323207888f68628bbefc39043c5249ed
Enric Morales <me@enric.me>
2017-02-02 13:27:51 +0100

    Preliminary urwid version

--- bin/sense.py
+++ bin/sense.py
@@ -3,13 +3,13 @@
 sens.py: log and output sensor values
 """
 
-import curses
 import statistics
 import time
-import collections
+import threading
 
 import psutil
 import sensors
+import urwid
 
 DATE_FMT = "%Y-%m-%d %H:%M:%S"
 QUIT_HINT = "Press 'q' to quit"
@@ -22,6 +22,9 @@ def init_history(chips):
     Build a history tree that will collect all the measurements.
     """
 
+    # Adapted from lm-sensors source code
+    sensor_units = ['', ' RPM', ' °C', '', '', '', '', ' V', ' ', '', '']
+
     tree = {}
     for chip in chips:
         sensor_dict = {}
@@ -29,19 +32,26 @@ def init_history(chips):
             if feature.label in BLACKLIST:
                 continue
             else:
-                sensor_dict[feature.label] = []
+                unit = sensor_units[feature.type]
+                sensor_dict[feature.label] = {}
+                sensor_dict[feature.label]['info'] = {'unit': unit}
+                sensor_dict[feature.label]['measurements'] = []
 
         tree[str(chip)] = sensor_dict
 
     tree['CPU Usage'] = {}
     for cpu in range(psutil.cpu_count()):
         core_id = 'Core #{}'.format(cpu)
-        tree['CPU Usage'][core_id] = []
+        tree['CPU Usage'][core_id] = {}
+        tree['CPU Usage'][core_id]['info'] = {'unit': '%'}
+        tree['CPU Usage'][core_id]['measurements'] = []
 
     tree['CPU Frequency'] = {}
     for cpu in range(psutil.cpu_count()):
         core_id = 'Core #{}'.format(cpu)
-        tree['CPU Frequency'][core_id] = []
+        tree['CPU Frequency'][core_id] = {}
+        tree['CPU Frequency'][core_id]['info'] = {'unit': ' MHz'}
+        tree['CPU Frequency'][core_id]['measurements'] = []
 
     return tree
 
@@ -54,18 +64,20 @@ def update_history(history, chips):
     for chip in chips:
         for feature in chip:
             try:
-                measurements = history[str(chip)][feature.label]
+                measurements = history[str(chip)][feature.label]['measurements']
                 measurements.append(feature.get_value())
             except KeyError:
                 continue
 
     for i, usage in enumerate(psutil.cpu_percent(percpu=True)):
         core_id = 'Core #{}'.format(i)
-        history['CPU Usage'][core_id].append(usage)
+        destination = history['CPU Usage'][core_id]['measurements']
+        destination.append(usage)
 
     for i, freq in enumerate(psutil.cpu_freq(percpu=True)):
         core_id = 'Core #{}'.format(i)
-        history['CPU Frequency'][core_id].append(float(round(freq.current)))
+        destination = history['CPU Frequency'][core_id]['measurements']
+        destination.append(float(round(freq.current)))
 
     return history
 
@@ -82,120 +94,86 @@ def format_number(number):
     return fmt
 
 
-def init_screen(screen, colors):
-    """
-    Initializes the screen for future use
-    """
-    # Hide cursor
-    curses.curs_set(0)
-
-    # Prepare screen and disable key delay
-    screen.clear()
-    screen.nodelay(True)
-
+def add_header():
     header = '{:<6} {:<6} {:<6} {:<6}'
     header = header.format('cur', 'min', 'max', 'avg')
-    screen.addstr(0, 18, header, colors.white)
+    return header
 
 
-def init_colors():
+def add_footer():
+    footer = "{:<16} {:<16} {:>16}"
+    footer = footer.format('sense.py',
+                           time.strftime(DATE_FMT),
+                           QUIT_HINT)
+
+    return footer
+
+def get_output(history):
     """
-    Initialize colors pairs and return a colors namedtuple
+    Make a list of strings out of sensor data.
     """
 
-    # Make color pairs
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    out = []
 
-    colors = collections.namedtuple("colors",
-                                    ["cyan", "yellow", "red", "green", "white"])
-    colors.cyan = curses.color_pair(1)
-    colors.green = curses.color_pair(2)
-    colors.red = curses.color_pair(3)
-    colors.yellow = curses.color_pair(4)
-    colors.white = curses.color_pair(5)
+    for chip in history:
+        # Print sensor name
+        out.append(str(chip))
 
-    return colors
+        features = history[chip]
+        for i, feature in enumerate(features):
+            # If this is the last element, print a different symbol
+            is_last = i + 1 == len(history[chip])
+            symbol = '\u2514' if is_last else '\u251c'
 
+            # Print sensor measurements
+            measurements = history[chip][feature]['measurements']
+            unit = history[chip][feature]['info']['unit']
+            data_fmt = '{:>6}{} {:>6}{} {:>6}{} {:>6.3f}{}'
+            data = data_fmt.format(format_number(measurements[-1]), unit,
+                                   format_number(min(measurements)), unit,
+                                   format_number(max(measurements)), unit,
+                                   statistics.mean(measurements), unit)
+            line = "{} {} {}".format(symbol, feature, data)
+            out.append(line)
 
-def print_footer(screen, colors):
-    max_y, max_x = screen.getmaxyx()
+        out.append("")
 
-    # Add a small footer
-    screen.addstr(max_y-1, 0,
-                  'sense.py',
-                  colors.cyan + curses.A_BOLD)
+    return out
 
-    # Add the date centered on the bottom
-    screen.addstr(max_y-1, 12, time.strftime(DATE_FMT),
-                  colors.green + curses.A_BOLD)
-    # And a hint to quit
-    screen.addstr(max_y-1, max_x-len(QUIT_HINT)-1, QUIT_HINT)
+def updater(loop, body, chips, history):
+    """ Loop that updates the content window. """
+    while True:
+        history = update_history(history, chips)
+        output = get_output(history)
+        body.set_text("\n".join(output))
+        loop.draw_screen()
+        time.sleep(1)
 
+def key_handler(key):
+    """ Handle keys such as Q for quit, etc."""
+    if key in ('q', 'Q'):
+        raise urwid.ExitMainLoop()
 
-def main(screen):
+def main():
     """
     Initialize screen, sensors, history and display measurements on the screen.
     """
-
-    colors = init_colors()
-    init_screen(screen, colors)
 
     # Initialize the sensors and the history
     sensors.init()
     chips = [s for s in sensors.iter_detected_chips()]
     history = init_history(chips)
 
-    while True:
-        # Quit on 'q' or 'Q' keypress
-        char = screen.getch()
-        if char == ord('q') or char == ord('Q'):
-            break
+    body = urwid.Text("")
+    fill = urwid.Filler(body, "top")
+    loop = urwid.MainLoop(fill, unhandled_input=key_handler)
 
-        line = 1
-        history = update_history(history, chips)
+    updater_thread = threading.Thread(target=updater,
+                                      args=(loop, body, chips, history))
 
-        for chip in history:
-            # Print sensor name
-            screen.addstr(line, 0, str(chip), colors.cyan)
-
-            features = history[chip]
-            for i, feature in enumerate(features):
-                line += 1
-                # Whether this is the last measurement from the chip
-                is_last = i + 1 == len(history[chip])
-
-                # Print tree drawing char
-                screen.addstr(line, 0,
-                              '\u2514' if is_last else '\u251c',
-                              colors.white)
-
-                # Print feature name
-                screen.addstr(line, 2, feature, colors.cyan + curses.A_BOLD)
-
-                # Print sensor measurements
-                measurements = history[chip][feature]
-                fmt = '{:<6} {:<6} {:<6} {:<6.3f}'
-                fmt = fmt.format(format_number(measurements[-1]),
-                                 format_number(min(measurements)),
-                                 format_number(max(measurements)),
-                                 statistics.mean(measurements)
-                                )
-                screen.addstr(line, 18, fmt, colors.green)
-
-            # Print a blank line
-            line += 2
-
-        print_footer(screen, colors)
-
-        # Put it together and sleep
-        screen.refresh()
-        time.sleep(1)
-
+    updater_thread.start()
+    loop.run()
 
 
 if __name__ == "__main__":
-    curses.wrapper(main)
+    main()
