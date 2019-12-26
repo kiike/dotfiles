71b33dc0c81047de790e3ca199a6b48d46256fa5
Enric Morales <me@enric.me>
2019-12-26 21:44:51 +0100

    Remove unneeded comments

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,14 +1,10 @@
--- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
 awful.rules = require("awful.rules")
 require("awful.autofocus")
--- Widget and layout library
 local wibox = require("wibox")
--- Theme handling library
 local beautiful = require("beautiful")
 local vicious = require("vicious")
--- Notification library
 local naughty = require("naughty")
 local hotkeys_popup = require("awful.hotkeys_popup").widget
 
@@ -17,11 +13,6 @@ local pomodoro = require("pomodoro")
 
 local typicons = require("typicons")
 
--- }}}
-
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
     naughty.notify({ preset = naughty.config.presets.critical,
                      title = "Startup errors:",
