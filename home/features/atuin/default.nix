ec8cfe5a7b0efbae245a1f2ef9e9657590514bd7
Enric Morales <me@enric.me>
2024-04-12 10:21:36 +0200

    fix: use atuin from git to allow using with nushell

--- home/features/atuin/default.nix
+++ home/features/atuin/default.nix
@@ -1,7 +1,12 @@
 {
+  inputs,
+  pkgs,
+  ...
+}: {
   programs.atuin = {
     enable = true;
     enableNushellIntegration = true;
+    package = inputs.atuin.packages.${pkgs.system}.default;
     settings = {
       key = "~/Documents/keys/atuin.key";
     };
