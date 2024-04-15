6279aad582f698cd977244b7ec9b88ed805a2a9e
Enric Morales <me@enric.me>
2024-04-15 22:37:20 +0200

    refactor(i18n): move to own module

--- hosts/dhalsim/default.nix
+++ hosts/dhalsim/default.nix
@@ -29,21 +29,6 @@
   # Set your time zone.
   time.timeZone = "Europe/Madrid";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_IE.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IE.UTF-8";
-    LC_IDENTIFICATION = "en_IE.UTF-8";
-    LC_MEASUREMENT = "en_IE.UTF-8";
-    LC_MONETARY = "en_IE.UTF-8";
-    LC_NAME = "en_IE.UTF-8";
-    LC_NUMERIC = "en_IE.UTF-8";
-    LC_PAPER = "en_IE.UTF-8";
-    LC_TELEPHONE = "en_IE.UTF-8";
-    LC_TIME = "en_IE.UTF-8";
-  };
-
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
