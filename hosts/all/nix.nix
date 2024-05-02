9547a26e98bc67325c6a1978655175b58f7ce600
Enric Morales <me@enric.me>
2024-05-02 18:20:34 +0200

    refactor(nix): move nix config to own file

---
+++ hosts/all/nix.nix
@@ -0,0 +1,25 @@
+{
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 15d";
+    };
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "repl-flake"
+      ];
+
+      trusted-substituters = [
+        "https://nix-community.cachix.org"
+        "https://helix.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      ];
+    };
+  };
+}
