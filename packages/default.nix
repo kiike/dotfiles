837e5ecdc4bbd8dd7e7eb217468c5868c69fc2b6
Enric Morales <me@enric.me>
2024-05-05 19:19:08 +0200

    feat: use hyprland-{activewindow,workspaces} from upstream nixpkgs

--- packages/default.nix
+++ packages/default.nix
@@ -3,6 +3,4 @@
 }:
 {
   symbols-nerd-font = pkgs.callPackage ./symbols-nerd-font { };
-  hyprland-activewindow = pkgs.callPackage ./hyprland-activewindow.nix { };
-  hyprland-workspaces = pkgs.callPackage ./hyprland-workspaces.nix { };
 }
