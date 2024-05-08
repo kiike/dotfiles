32c561fea8830c4a3489b6c2292dc3cc17005bf9
Enric Morales <me@enric.me>
2024-05-08 19:22:35 +0200

    feat(dhalsim): stop using nvidia settings

--- hosts/dhalsim/default.nix
+++ hosts/dhalsim/default.nix
@@ -116,7 +116,6 @@
     modesetting.enable = true;
     powerManagement.enable = true;
     open = false;
-    nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
