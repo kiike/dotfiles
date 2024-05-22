1a0578c2d39cdb7f38b6daed474d3cbd395b684e
Enric Morales <me@enric.me>
2024-05-22 11:38:45 +0200

    build: add deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+set -eu
+
+deploy_system() {
+    sudo nixos-rebuild switch --flake ~/dotfiles
+}
+
+deploy_home() {
+    home-manager switch --flake ~/dotfiles -b backup
+}
+
+target=all
+if [ $# -gt 0 ]; then
+  target=$1
+  shift
+fi
+case $target in
+  system|s) deploy_system ;;
+  home|h) deploy_home ;;
+  all|*)
+    deploy_system
+    deploy_home
+    ;;
+esac
