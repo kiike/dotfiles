10079a8ac5c9e715c65b483e2c20be49cf8c9f5f
Enric Morales <me@enric.me>
2024-06-06 21:00:50 +0200

    feat(reaper): wrap program in rootful Xwayland to work around glitches

--- home/features/proaudio.nix
+++ home/features/proaudio.nix
@@ -1,7 +1,27 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  wrapperName = "reaper-session";
+in
 {
   home.packages = with pkgs; [
+
     reaper
     qjackctl
+    (writeScriptBin wrapperName ''
+      #!/usr/bin/env nix-shell
+      #! nix-shell -i bash -p bash icewm
+
+      DISPLAY=:$(${lib.getExe xwayland} -reset -terminate -displayfd 1 &)
+      echo $DISPLAY
+      ${lib.getExe' reaper "reaper"} &
+      exec ${lib.getExe' icewm "icewm-session"} &
+      wait $server
+    '')
+    (pkgs.makeDesktopItem {
+      desktopName = wrapperName;
+      name = wrapperName;
+      exec = wrapperName;
+      icon = "cockos-reaper";
+    })
   ];
 }
