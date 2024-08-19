a4fc1fccc1132d9a1b3f8013ceec753381518fba
Enric Morales <me@enric.me>
2024-08-19 18:33:07 +0200

    refactor(kitty): remove alacritty bit

--- home/features/desktop/kitty.nix
+++ home/features/desktop/kitty.nix
@@ -79,17 +79,4 @@
     '';
   };
 
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      shell = "nu";
-      font.normal.family = "SauceCodePro NF Medium";
-      window.opacity = 0.8;
-      bell = {
-        duration = 1;
-        color = "#ff0000";
-        animation = "EaseOut";
-      };
-    };
-  };
 }
