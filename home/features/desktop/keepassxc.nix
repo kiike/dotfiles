f889320d4f2ed0ec0372f66d72de7aaa25ce1a92
Enric Morales <me@enric.me>
2024-09-04 12:08:37 +0200

    feat: add keepassxc service

--- home/features/desktop/keepassxc.nix
+++ home/features/desktop/keepassxc.nix
@@ -1,4 +1,22 @@
 { pkgs, ... }:
 {
   home.packages = [ pkgs.keepassxc ];
+  systemd.user.services."keepassxc" = {
+    Unit = {
+      PartOf = "graphical-session.target";
+      After = [
+        "graphical-session.target"
+        "tray.target"
+      ];
+      Requisite = "graphical-session.target";
+    };
+    Service = {
+      # Delay start by a bit to let the tray settle
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 2s";
+      ExecStart = "${pkgs.keepassxc}/bin/keepassxc";
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
 }
