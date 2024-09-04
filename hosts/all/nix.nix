855fb3842987a00b79c7870186c185d12848eacb
Enric Morales <me@enric.me>
2024-09-04 12:16:51 +0200

    feat: add garnix cache

--- hosts/all/nix.nix
+++ hosts/all/nix.nix
@@ -17,10 +17,12 @@
       trusted-substituters = [
         "https://nix-community.cachix.org"
         "https://helix.cachix.org"
+        "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
   };
