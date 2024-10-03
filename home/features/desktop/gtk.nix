a8c302f53c37fe39b61ec2deb4ff01895c335588
Enric Morales <me@enric.me>
2024-10-03 10:13:30 +0200

    fix(gtk): remove hyprland portal

--- home/features/desktop/gtk.nix
+++ home/features/desktop/gtk.nix
@@ -5,9 +5,6 @@
     enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     config = {
-      common = {
-        default = [ "hyprland" ];
-      };
       gtk = {
         "org.freedesktop.impl.portal.Settings" = [ "gtk" ];
       };
